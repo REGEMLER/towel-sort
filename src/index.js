b4394c96a660ff7e86b39e47b9942eeff38f392b
Yar Night <yaroslav239@gmail.com>
2023-08-13 19:48:34 +0300

    feat: tast implement

--- src/index.js
+++ src/index.js
@@ -2,5 +2,17 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if(!matrix || matrix.length === 0){
+    return []
+  }
+  let result = []; 
+  matrix.forEach((item, index) => {
+    if(index % 2 === 0){
+      item.forEach(i => result.push(i))
+    } else {
+      let newItem = item.reverse();
+      newItem.forEach(i => result.push(i))
+    }
+  })
+  return result; 
 }
